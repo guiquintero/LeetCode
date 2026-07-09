4dc780d5413f07a374b6a9b222a3b5cb9af72a9f
Guilherme Quintero <guiquintero02@gmail.com>
2026-07-09 13:13:22 +0100

    Refactor canPlaceFlowers function for improved logic and clarity

--- 605. Can Place Flowers.ts
+++ 605. Can Place Flowers.ts
@@ -1,24 +1,59 @@
 function canPlaceFlowers(flowerbed: number[], n: number): boolean {
     
-    let t1 = 0, count = 0;
+    let z=0, u=0, count = 0, t=flowerbed.length;
 
+    for(let i=0;i<t;i++){
+        if(flowerbed[i]==0){
+            z+=1;
+            if(u>=1 && i==t-1){
+                count+=Math.floor(z/2);
+            } else if(u==0 && i==t-1){
+                count+=Math.ceil(z/2);
+            }
+        } else{
+            u+=1;
+            if(u==1){
+                count+=Math.floor(z/2);
+            }else{
+                count+=Math.ceil((z-2)/2);
+            }
+            z=0;
+        }
+    }
+    return count>=n;
+};
+
+console.log(canPlaceFlowers([1,0,1,0,1,0,1], 1));
+
+// 1=0  1=0
+// 2=0  2=1
+// 3=1  3=1
+// 4=1  4=2
+// 5=2
+// 6=2
+// 7=3
+// 8=3
+// 9=4
+// 10=4
+// 11=5
+
+
+
+function canPlaceFlowers1(flowerbed: number[], n: number): boolean {
+    
+    let t1 = -1, count = 0, m=0;
 
     for(let i=0;i<flowerbed.length;i++){
         if(flowerbed[i]==1){
-            count = i-t1;
-            
-
-
+            m=Math.floor((i-t1-2)/2)
+            if(t1==-1){
+                count+= Math.floor(i/2)
+            }else{
+                count += m;
+            }
+            t1 = i;  
+            console.log(count)
         }
     }
-
-    return true;
+    return count>=n;
 };
-
-3=1 1 0 0 0 1
-4=1
-5=2
-6=2
-7=3
-8=3
-
